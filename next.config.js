42febde93398d2610a853f13f534f952b62478c4
Luke Carter <everythingsimpleinc1@gmail.com>
2025-08-09 07:06:09 +0100

    chore: stabilize Vercel build (ignore eslint warnings; unoptimized local images; reduce file tracing scope)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    // Build should not fail on lint warnings in CI
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // We only use local images; avoid remote loader setup and reduce tracing
+    unoptimized: true,
+  },
+  experimental: {
+    // Reduce the scope of file tracing to avoid micromatch recursion issues on some environments
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/**',
+        '.git/**',
+        'scripts/**',
+      ],
+    },
+  },
+}
 
 module.exports = nextConfig
